01f08317185500b79b08831585a6f83999641988
AFBGdev <anresfb@gmail.com>
2024-11-18 07:30:00 -0500

    feat: :sparkles: add the availability to list all links

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -14,7 +14,27 @@ class LinkController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $links = Link::all();
+
+            $responseData = [
+                "status" => "success",
+                "data" => $links
+            ];
+
+            return response()->json($responseData);
+
+        } catch (\Exception $error) {
+            Log::error($error->getMessage());
+
+            return response()->json([
+                "status" => "error",
+                "error" => [
+                    "code" => "b28e75a8-28f2-40c0-8627-ebf443831627",
+                    "message" => "Internal server error!."
+                    ]
+                ], 500);
+        }
     }
 
     /**
